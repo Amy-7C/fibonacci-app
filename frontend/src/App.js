447192eec1ee2dcb8886bc9d3ab332fe523b61d6
amy-b12 <back.amy12@gmail.com>
2023-07-29 17:16:56 -0400

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Fibonacci Number Generator</h1>
+        <p>Input a number 'n' to display the first 'n' fibonacci numbers.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
