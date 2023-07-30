778df14c52b4003c6785dfb4b4fa7c3c29f97211
amy-b12 <back.amy12@gmail.com>
2023-07-29 22:38:45 -0400

    created simple form on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Fibonacci Number Generator</h1>
         <p>Input a number 'n' to display the first 'n' fibonacci numbers.</p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={input} onChange={(e) => setInput(e.target.value)}></input>
+          <button type="submit">Get Results</button>
+        </form>
       </header>
     </div>
   );
